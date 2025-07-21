1dacde2a7c439698b844d389e576b6e371d03528
Satyajit Behera <satyabehera986@gmail.com>
2025-07-21 09:15:02 +0530

    Update AuthController.java

--- AuthController.java
+++ AuthController.java
@@ -34,18 +34,14 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request) {
-        // Authenticate user credentials
         authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword())
         );
 
-        // Fetch user from database
         User user = ( userRepository.findByUsername(request.getUsername())).orElseThrow();
 
-        // Generate JWT token
         String token = jwtUtil.generateToken(user.getUsername(), user.getRole());
 
-        // Return token in response
         return ResponseEntity.ok(new AuthResponse(token));
     }
 }
